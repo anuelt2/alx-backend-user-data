88961f433cdb9f3a6643eabda3efd5487d718a7c
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-05-21 21:57:33 +0000

    Add updated filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """filtered_logger module"""
 
+import logging
 import re
 from typing import List
 
@@ -14,3 +15,27 @@ def filter_datum(
     """Returns obfuscated log message"""
     pattern = r"(" + "|".join(fields) + r")=.*?" + re.escape(separator)
     return re.sub(pattern, r"\1=" + redaction + separator, message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Filter values in incoming log records using `filter_datum`
+        """
+        log_message = super().format(record)
+        return filter_datum(
+                self.fields,
+                self.REDACTION,
+                log_message,
+                self.SEPARATOR
+                )
