3a828e80f8feb2a93a5041e68e1a42fdc739b99a
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-05-22 08:24:18 +0000

    Add get_db function to filtered_logger module

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """filtered_logger module"""
 
+import os
 import logging
+import mysql.connector
 import re
 from typing import List
 
@@ -58,3 +60,20 @@ def get_logger() -> logging.Logger:
     logger.addHandler(stream_handler)
 
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Returns a connector to the database"""
+    db_username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    db_password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
+
+    connection = mysql.connector.connect(
+            host=db_host,
+            user=db_username,
+            password=db_password,
+            database=db_name
+            )
+
+    return connection
