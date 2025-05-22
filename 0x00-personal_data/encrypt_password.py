c48e1153fddcf440c3c27a052210fc28efefe7bc
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-05-22 10:17:32 +0000

    Add encrypt_password.py

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""encrypt_password module"""
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Takes a password and returns a salted, hashed password as byte string"""
+    password_bytes = password.encode("utf-8")
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password_bytes, salt)
+
+    return hashed
