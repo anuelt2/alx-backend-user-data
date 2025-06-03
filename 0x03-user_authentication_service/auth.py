7f83928e128e9c5e0e81ac273786365a424a28bd
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-06-03 15:52:34 +0000

    Implement Auth.get_reset_password_token method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -77,3 +77,13 @@ class Auth:
     def destroy_session(self, user_id: int) -> None:
         """Sets User session ID to None"""
         self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Generate and return a password reset token"""
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+        except NoResultFound:
+            raise ValueError
